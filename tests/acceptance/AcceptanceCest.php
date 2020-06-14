90d114764ea673f6df70e1e8db98de0b3207e31d
Clemens Tögel <Clemens.Toegel@gmx.at>
2020-06-14 08:25:05 +0200

    New test cases

--- tests/acceptance/AcceptanceCest.php
+++ tests/acceptance/AcceptanceCest.php
@@ -7,9 +7,22 @@ class AcceptanceCest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function isOnStartPage(AcceptanceTester $I)
     {
         $I->amOnPage('/');
         $I->see('Continuous-Delivery Blog');
     }
+
+    public function databaseCreatedTest(AcceptanceTester $I){
+        $I->seeInDatabase("article", ["id" => "1", "category" => "1", "title" => "Title First Article"]);
+    }
+
+    public function clicksOnPolitics(AcceptanceTester $I){
+        // simple link
+        $I->amOnPage('/');
+        $I->click('Politics');
+        $I->see('Second Test Article');
+    }
+
+
 }
