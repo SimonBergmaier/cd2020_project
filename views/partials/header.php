271f158973e989ee4066f841dc5d75d0c3f221d5
Simon Bergmaier <simon.bergmaier@gmail.com>
2020-06-15 15:31:45 +0200

    removed need for bootstrap-theme

--- views/partials/header.php
+++ views/partials/header.php
@@ -17,7 +17,6 @@ if (isset($_GET["errors"])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
-	<link href="vendor/twbs/bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
 	<link href="assets/main.css" rel="stylesheet">
 	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" defer></script>
     <script src="https://unpkg.com/@popperjs/core@2" defer></script>
