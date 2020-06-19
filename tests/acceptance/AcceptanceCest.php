9bb84c623a36838d99d0cb107c4a8c88b09598fa
Clemens Tögel <Clemens.Toegel@gmx.at>
2020-06-19 13:03:51 +0200

    Further acceptance tests

--- tests/acceptance/AcceptanceCest.php
+++ tests/acceptance/AcceptanceCest.php
@@ -4,59 +4,95 @@ class AcceptanceCest
 {
 
     private $username = "FirstUser";
+    private $userId = "1";
     private $cookie = null;
 
     public function _before(AcceptanceTester $I)
     {
     }
 
-    // tests
+    /**
+     * Tests if the users sees the landing page
+     * @param AcceptanceTester $I
+     */
     public function IsOnStartpage(AcceptanceTester $I)
     {
         $I->amOnPage('/');
         $I->see('Continuous-Delivery Blog');
+        $I->see('Please select a category');
     }
 
-    public function IsDatabaseCreated(AcceptanceTester $I){
+    /**
+     * Tests whether the database was created correct or not
+     * @param AcceptanceTester $I
+     */
+    public function IsDatabaseCreated(AcceptanceTester $I)
+    {
         $I->seeInDatabase("categories", ["id" => "1", "name" => "Politics"]);
         $I->seeInDatabase("article", ["id" => "1", "category" => "1", "title" => "Title First Article"]);
         $I->seeInDatabase("users", ["id" => "1", "username" => "FirstUser"]);
         $I->seeInDatabase("comment", ["id" => "1", "articleId" => "2", "authorId" => "1", "text" => "First Comment Second Article"]);
     }
 
-    public function isLoginWorking(AcceptanceTester $I){
+    /**
+     * Tests if the login workflow is working
+     * and sets the PHPSESSID cookie, so that it can be used to test other functionalities,
+     * which requires logged in users
+     * @param AcceptanceTester $I ,
+     */
+    public function isLoginWorking(AcceptanceTester $I)
+    {
         $I->amOnPage('/');
         $I->see('Not logged in!');
         $I->click('Not logged in!');
         $I->see('Login now');
         $I->click('Login now');
         $I->fillField('userName', $this->username);
-        $I->fillField('password','Testuser');
+        $I->fillField('password', 'Testuser');
         $I->click('loginbtn');
         $I->see('Logged in as ' . $this->username);
-        $this->cookie   = $I->grabCookie('PHPSESSID');
+        $this->cookie = $I->grabCookie('PHPSESSID');
     }
 
-    public function clicksOnFirstCategory(AcceptanceTester $I){
+    /**
+     * Tests if the users sees the articles of the first category
+     * (Articles in second category 2)
+     * @param AcceptanceTester $I
+     */
+    public function clicksOnFirstCategory(AcceptanceTester $I)
+    {
+        $XPATH_ARTICLE_IN_LIST = 'tbody/tr';
         $category_id = 1;
         $I->amOnPage('/');
         $category = $I->grabFromDatabase('categories', 'name', ['id' => $category_id]);
         $I->click($category);
         $I->amOnPage('index.php/?view=list&categoryId=' . $category_id);
         $I->see($I->grabFromDatabase('article', 'title', array(['category' => $category_id])[0]));
+        $arrayProducts = $I->grabMultiple($XPATH_ARTICLE_IN_LIST);
+        $sumProducts = count($arrayProducts);
+        $I->seeNumberOfElements($XPATH_ARTICLE_IN_LIST, $sumProducts);
     }
 
-    public function clicksOnEmptyCategory(AcceptanceTester $I){
+    /**
+     * Tests if the users sees the notification that no articles are in the second category
+     * @param AcceptanceTester $I
+     */
+    public function clicksOnSecondCategoryWhichIsEmpty(AcceptanceTester $I)
+    {
         $category_id = 2;
         $I->amOnPage('/');
-        $category = $I->grabFromDatabase('categories', 'name', ['id' =>  $category_id]);
+        $category = $I->grabFromDatabase('categories', 'name', ['id' => $category_id]);
         $I->click($category);
         $I->amOnPage('index.php/?view=list&categoryId=' . $category_id);
         $I->see('No articles in this category');
     }
 
-    public function clicksOnViewFirstArticle(AcceptanceTester $I){
-        $category_id = 1;
+    /**
+     * Tests if user see content of second article from first category
+     * @param AcceptanceTester $I
+     */
+    public function clicksOnViewSecondArticle(AcceptanceTester $I)
+    {
         $article_id = 2;
         $I->amOnPage('index.php/?view=list&categoryId=' . $category_id = 1);
         $I->click("View Article");
@@ -64,19 +100,69 @@ class AcceptanceCest
         $I->see($I->grabFromDatabase('article', 'title', ['id' => $article_id]));
     }
 
-    public function CommentWithoutLogin(AcceptanceTester $I){
+    /**
+     * Tests if the user sees the comment on the second article
+     * @depends clicksOnViewSecondArticle
+     * @param AcceptanceTester $I
+     */
+    public function seeCommentsOnSecondArticle(AcceptanceTester $I)
+    {
+        $CSS_COMMENTS_ON_ARTICLE = 'panel';
+        $article_id = 2;
+        $I->amOnPage('index.php?view=article&id=' . $article_id);
+        $I->see($I->grabFromDatabase('article', 'title', ['id' => $article_id]));
+        $I->see($I->grabFromDatabase('comment', 'text', ['articleId' => $article_id]));
+        $arrayProducts = $I->grabMultiple($CSS_COMMENTS_ON_ARTICLE);
+        $sumProducts = count($arrayProducts);
+        $I->comment($sumProducts);
+        $I->seeNumberOfElements($CSS_COMMENTS_ON_ARTICLE, $sumProducts);
+    }
+
+    /**
+     * Tests if the user can edit his/her article
+     * (UserId = 1, articleId = 2)
+     * @depends isLoginWorking
+     * @param AcceptanceTester $I
+     */
+    public function editSecondArticleWhenLoggedInWithCorrectUser(AcceptanceTester $I)
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+        $article_id = 2;
+        $I->amOnPage('index.php?view=article&id=' . $article_id);
+        $I->see($I->grabFromDatabase('article', 'title', ['id' => $article_id]));
+        $I->see("Edit Article");
+        $I->see("Delete Article");
+    }
+
+    /**
+     * Tests if the user can edit his/her comments on the second article
+     * (UserId = 1, articleId = 2)
+     * @depends isLoginWorking
+     * @param AcceptanceTester $I
+     */
+    public function editCommentsOfSecondArticleWhenLoggedInWithCorrectUser(AcceptanceTester $I)
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+        $article_id = 2;
+        $I->amOnPage('index.php?view=article&id=' . $article_id);
+        $I->see($I->grabFromDatabase('article', 'title', ['id' => $article_id]));
+        $I->see($I->grabFromDatabase('comment', 'text', ['articleId' => $article_id, 'authorId' => 1]));
+        $I->see("Edit Comment");
+    }
+
+    public function CommentWithoutLogin(AcceptanceTester $I)
+    {
         $article_id = 3;
         $I->amOnPage('index.php?view=article&id=' . $article_id);
         $I->dontSee('Create Comment:');
-        //$I->fillField('text', 'Hello this is a new Comment on ' . $I->grabFromDatabase('article', 'title', ['id' => $article_id]));
-        //$I->see('Not logged in.');
     }
 
     /**
      * @depends isLoginWorking
      * @param AcceptanceTester $I
      */
-    public function CommentWithLogin(AcceptanceTester $I){
+    public function CommentWithLogin(AcceptanceTester $I)
+    {
         $I->setCookie('PHPSESSID', $this->cookie);
         $article_id = 3;
         $I->amOnPage('index.php?view=article&id=' . $article_id);
@@ -85,15 +171,25 @@ class AcceptanceCest
         $I->fillField('#inputText', $comment);
         $I->click('Create Comment...');
         $I->seeInDatabase("comment", ["text" => $comment]);
-
         $I->see($comment);
     }
 
-    public function NewArticleWithoutLogin(AcceptanceTester $I){
+    public function NewArticleWithoutLogin(AcceptanceTester $I)
+    {
         $I->amOnPage('index.php');
         $I->dontSee('Create new Article');
     }
 
+    /**
+     * @depends isLoginWorking
+     * @param AcceptanceTester $I
+     */
+    public function NewArticleWithLogin(AcceptanceTester $I)
+    {
+        $I->setCookie('PHPSESSID', $this->cookie);
+        $I->amOnPage('index.php');
+        $I->see('Create new Article');
+    }
 
 
 }
